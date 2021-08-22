69189762c2c7bdbd5051f50bdf12b1b8195cfc95
luoyu <liangzhengtao.lzt@lazada.com>
2021-08-22 15:13:20 +0800

    [feat] 引入日志

--- springInitializrDemo/src/test/java/com/luoyu/springinitializrdemo/SpringInitializrDemoApplicationTests.java
+++ springInitializrDemo/src/test/java/com/luoyu/springinitializrdemo/SpringInitializrDemoApplicationTests.java
@@ -2,6 +2,8 @@ package com.luoyu.springinitializrdemo;
 
 import com.luoyu.springinitializrdemo.bean.Person;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
@@ -14,6 +16,8 @@ import java.time.Period;
  **/
 @SpringBootTest
 class SpringInitializrDemoApplicationTests {
+
+    Logger logger = LoggerFactory.getLogger(SpringInitializrDemoApplicationTests.class);
 
     @Autowired
     Person person;
@@ -30,6 +34,17 @@ class SpringInitializrDemoApplicationTests {
     @Test
     void contextLoads() {
         System.out.println(person);
+
+        //日志的级别
+        //从低到高
+
+        logger.trace("this trace");
+        logger.debug("this debug");
+        //SpringBoot默认给我们使用的是info级别的日志
+        logger.info("this info");
+        logger.warn("this warn");
+        logger.error("this error");
+
     }
 
 }
